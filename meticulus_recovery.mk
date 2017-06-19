96185cc4ec46baccadae62e0b5916a8dd8308b65
Meticulus <theonejohnnyd@gmail.com>
2017-06-19 15:32:31 -0500

    vendor: separate recovery blobs

---
+++ meticulus_recovery.mk
@@ -0,0 +1,6 @@
+# Meticulus Development: Extras
+PRODUCT_COPY_FILES += \
+    vendor/huawei/hi6250/recovery/sbin/supersu.zip:recovery/root/sbin/supersu.zip \
+    vendor/huawei/hi6250/recovery/sbin/StockBackup_hi6250.zip:recovery/root/sbin/StockBackup_hi6250.zip \
+    vendor/huawei/hi6250/recovery/sbin/RestoreStockBackup_hi6250.zip:recovery/root/sbin/RestoreStockBackup_hi6250.zip
+
